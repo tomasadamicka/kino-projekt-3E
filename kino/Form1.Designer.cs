0c6d99005b85d8fcce13bb348d6796d64fb3916e
michelj <michelj@312-16>
2024-09-27 09:22:05 +0200

    nv

--- kino/Form1.Designer.cs
+++ kino/Form1.Designer.cs
@@ -28,47 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "KINO ŠVANDA";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(307, 190);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "EJDEMIČKA";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
--- kino/Form1.cs
+++ kino/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +19,76 @@ namespace kino
             InitializeComponent();
             //adamickatomas
         }
+            SQLiteConnection sqlite_c;
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                int i, j;
+                for (i = 0; i < 3; i++)
+                {
+                    for (j = 0; j < 3; j++)
+                    {
+                        Button bt = new Button();
+                        bt.Top = 50 * i;
+                        bt.Left = 50 * j;
+                        bt.Width = velikost;
+                        bt.Name = i.ToString() + j.ToString();
+                        bt.Height = velikost;
+                        this.Controls.Add(bt);
+                    }
+                }
+            }
+            int velikost = 200;
+            private SQLiteConnection CreateConnection()
+            {
+                SQLiteConnection sqlite_c;
+                // Create a new database connection:
+                sqlite_c = new SQLiteConnection("Data Source = database1.db; Version = 3; New = True; Compress = True; ");
+                // Open the connection:
+                try
+                {
+                    sqlite_c.Open();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                return sqlite_c;
+            }
+            private void CreateTable(SQLiteConnection conn)
+            {
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = conn.CreateCommand();
+                string Createsql = @"CREATE TABLE IF NOT EXISTS automat
+                   (id INT AUTO_INCREMENT PRIMARY KEY, kod VARCHAR(10), nazev VARCHAR(10), cena INT, pocet INT)";
+                sqlite_cmd.CommandText = Createsql;
+                sqlite_cmd.ExecuteNonQuery();
+            }
+
+            private void InsertData(SQLiteConnection conn, string kod, string nazev, int cena, int pocet)
+            {
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = conn.CreateCommand();
+                sqlite_cmd.CommandText = @"INSERT INTO automat
+                   (kod, nazev, cena, pocet) VALUES('" + kod + "','" + nazev + "','" + cena + "','" + pocet + "'); ";
+                sqlite_cmd.ExecuteNonQuery();
+            }
+
+            private string ReadData(SQLiteConnection conn)
+            {
+                string text = "";
+                SQLiteDataReader sqlite_datareader;
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = conn.CreateCommand();
+                sqlite_cmd.CommandText = "SELECT * FROM automat";
+
+                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                while (sqlite_datareader.Read())
+                {
+                    text += sqlite_datareader.GetString(1) + "," + sqlite_datareader.GetString(2) + "," + sqlite_datareader.GetString(3) + "\n";
+                }
+                return (text);
+                //conn.Close();
+            }
+        }
     }
-}
